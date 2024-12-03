52f77e6ec12d4784e1d55001ed89ad5ecb503474
Sophia <133078271+HerSophia@users.noreply.github.com>
2024-12-04 00:29:58 +0800

    Update Run API.bat

--- Run API.bat
+++ Run API.bat
@@ -1,17 +1,17 @@
 @echo off
-REM ����Ƕ��� Python ������·��
+REM 设置嵌入的 Python 解释器路径
 set PYTHON_PATH=.\python\python.exe
 
-REM ����֪ʶͼ��
-echo [��Ϣ] ���ڹ���֪ʶͼ��...
-"%PYTHON_PATH%" lightrag_api_openai_compatible.py
+REM 构建知识图谱
+echo [信息] 正在构建知识图谱...
+"%PYTHON_PATH%" ".\lightrag_api_openai_compatible.py"
 if errorlevel 1 (
-    echo [����] ���з���ʧ�ܣ���鿴�׳��Ĵ��󲢽����Ų飬���߼���������ӡ�
+    echo [错误] 运行服务失败，请查看抛出的错误并进行排查，或者检查网络连接。
     pause
     exit /b
 )
 
-REM ��ʾ���
-echo [�ɹ�] ���гɹ����ɿ�ʼ���Ӿƹݣ�
-echo [��ʾ] ��ķ���˿�Ϊ���鿴README���ھƹ��������URL
+REM 提示完成
+echo [成功] 运行成功，可开始连接酒馆！
+echo [提示] 你的服务端口为，查看README以在酒馆中填入该URL
 pause
