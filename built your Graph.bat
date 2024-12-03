9381dcfd613a34646eeb5327f073ee274049651a
Sophia <133078271+HerSophia@users.noreply.github.com>
2024-12-04 00:32:35 +0800

    Add files via upload

--- built your Graph.bat
+++ built your Graph.bat
@@ -1,16 +1,16 @@
 @echo off
-REM 设置嵌入的 Python 解释器路径
+REM ����Ƕ��� Python ������·��
 set PYTHON_PATH=.\python\python.exe
 
-REM 构建知识图谱
-echo [信息] 正在构建知识图谱...
+REM ����֪ʶͼ��
+echo [��Ϣ] ���ڹ���֪ʶͼ��...
 "%PYTHON_PATH%" ".\built your Graph.py"
 if errorlevel 1 (
-    echo [错误] 构建知识图谱失败，请查看抛出的错误并进行排查，或者检查网络连接。
+    echo [����] ����֪ʶͼ��ʧ�ܣ���鿴�׳��Ĵ��󲢽����Ų飬���߼���������ӡ�
     pause
     exit /b
 )
 
-REM 提示完成
-echo [成功] 构建完成，可开始运行服务！
+REM ��ʾ���
+echo [�ɹ�] ������ɣ��ɿ�ʼ���з���
 pause
